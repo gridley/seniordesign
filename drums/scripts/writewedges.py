6018542cded8b51cd2fc64762c7305c65f71cd31
Gavin Ridley <gridley@vols.utk.edu>
2018-03-23 00:34:24 -0400

    wedge writer

---
+++ drums/scripts/writewedges.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Units: CGS
+from jinja2 import Template
+import numpy as np
+
+sin30 = np.sin(30.0 * np.pi / 180.0)
+cos30 = np.cos(30.0 * np.pi / 180.0)
+clear = .01 # cm, default ISO H11 clearance for ~ this geom
+
+## Write wedges that go between core and drums; thus more efficiently reflects
+#  neutrons
+r2drum = 13.5 
+rdrum = 6.735
+rvess = 6.635
+
+with open('wedge.template') as fh:
+    templatetext = fh.read()
+
+t = Template(templatetext)
+tt = t.render(**locals())
+
+print(tt)
